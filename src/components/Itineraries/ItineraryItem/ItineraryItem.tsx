a604f38f1f27bd5145b9b6e74254b4d75da3a895
Russell Arvin <56810871+RussellArvin@users.noreply.github.com>
2024-02-05 23:37:47 +0800

    fix: integrate backend with main page

--- src/components/Itineraries/ItineraryItem/ItineraryItem.tsx
+++ src/components/Itineraries/ItineraryItem/ItineraryItem.tsx
@@ -20,17 +20,26 @@ const Item = styled(Paper)(({ theme }) => ({
   cursor: "pointer",
 }));
 
+const cityImages = new Map([
+  ["Jakarta", "https://s7g10.scene7.com/is/image/barcelo/things-to-do-in-jakarta_jakarta-tourist-spots?&&fmt=webp-alpha&qlt=75&wid=1300&fit=crop,1"],
+  ["New York", "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg/800px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg"],
+  ["Seoul", "https://static.independent.co.uk/2022/12/29/14/iStock-464629385.jpg"],
+  ["Taipei", "https://res.klook.com/image/upload/fl_lossy.progressive,w_800,c_fill,q_85/Taipei_CP1125X624_1.jpg"],
+  ["Tokyo", "https://media.cntraveller.com/photos/6343df288d5d266e2e66f082/16:9/w_2560%2Cc_limit/tokyoGettyImages-1031467664.jpeg"],
+]);
 
 
-export default function ItineraryItem({ itineraryDetail, cityImage }: ItineraryItemType) {
-  const { location, startBudget, endBudget, duration, pax, rating } = itineraryDetail;
+
+export default function ItineraryItem(details: ItineraryItemType) {
+  const { location, startBudget, endBudget, duration, groupSize } = details
+
   return (
     <Grid item xs={4} md={4}>
       <Item>
         <Card sx={{ maxWidth: 345 }}>
           <CardMedia
             sx={{ height: 160 }}
-            image={cityImage}
+            image={cityImages.get(location)}
             title="city"
           />
           <CardContent>
@@ -45,14 +54,8 @@ export default function ItineraryItem({ itineraryDetail, cityImage }: ItineraryI
                 {duration} days
               </Typography>
               <Typography>
-                {pax} pax
+                {groupSize} pax
               </Typography>
-              {rating ?
-                <IconButton color="secondary" aria-label="add an alarm">
-                  <ThumbUpIcon />
-                </IconButton> : <IconButton color="secondary" aria-label="add an alarm">
-                  <ThumbDownIcon />
-                </IconButton>}
             </Container>
           </CardContent>
         </Card>
--- src/components/Itineraries/ItineraryList/ItineraryList.tsx
+++ src/components/Itineraries/ItineraryList/ItineraryList.tsx
@@ -3,41 +3,23 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import ItineraryItem from '../ItineraryItem/ItineraryItem';
 import { ItineraryItemType } from '../../../utils/types';
-import { uuid } from "uuidv4";
+import { uuid } from 'uuidv4';
 
-const cityImages = new Map([
-    ["Jakarta", "https://s7g10.scene7.com/is/image/barcelo/things-to-do-in-jakarta_jakarta-tourist-spots?&&fmt=webp-alpha&qlt=75&wid=1300&fit=crop,1"],
-    ["New York", "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg/800px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg"],
-    ["Seoul", "https://static.independent.co.uk/2022/12/29/14/iStock-464629385.jpg"],
-    ["Taipei", "https://res.klook.com/image/upload/fl_lossy.progressive,w_800,c_fill,q_85/Taipei_CP1125X624_1.jpg"],
-    ["Tokyo", "https://media.cntraveller.com/photos/6343df288d5d266e2e66f082/16:9/w_2560%2Cc_limit/tokyoGettyImages-1031467664.jpeg"],
-]);
-
-
-const DUMMY_ARR: ItineraryItemType[] = [
-    { location: "New York", startBudget: 1000, endBudget: 4000, duration: 3, pax: 4, rating: true },
-    { location: "Tokyo", startBudget: 1000, endBudget: 4000, duration: 3, pax: 4, rating: true },
-    { location: "Jakarta", startBudget: 1000, endBudget: 4000, duration: 3, pax: 4, rating: false },
-    { location: "Taipei", startBudget: 1000, endBudget: 4000, duration: 3, pax: 4, rating: true },
-    { location: "Seoul", startBudget: 1000, endBudget: 4000, duration: 3, pax: 4, rating: true }
-];
-
-
-
-const itineraries = DUMMY_ARR.map((itinerary: ItineraryItemType) => (
-    <ItineraryItem
-      key={uuid()}
-      cityImage={cityImages.get(itinerary.location)}
-      itineraryDetail={itinerary} // Corrected typo: itineratDetails to itineraryDetails
-    />
-  ));
-
-
-export default function ItineraryList() {
+export default function ItineraryList({ itineraries }: { itineraries: ItineraryItemType[] }) {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <Grid container spacing={2}>
-                {itineraries}
+                {itineraries.map((itinerary: ItineraryItemType) => (
+                    <ItineraryItem
+                    id={itinerary.id}
+                    userId={itinerary.userId}
+                    location={itinerary.location}
+                    startBudget={itinerary.startBudget}
+                    endBudget={itinerary.endBudget}
+                    duration={itinerary.duration}
+                    groupSize={itinerary.groupSize}
+                    />
+                ))}
             </Grid>
         </Box>
     );
